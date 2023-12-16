2914924604ba3ff71c0967b95245e4b44f39938b
Daisuke Aritomo <osyoyu@osyoyu.com>
2023-12-17 01:26:41 +0900

    Display published date if no title

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,8 @@
 class Entry < ApplicationRecord
+  def display_title
+    title.nil? ? published_at_formatted : title
+  end
+
   def published_at_formatted
     published_at.strftime('%Y/%m/%d')
   end
