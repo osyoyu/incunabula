735a334748ff65f129a794f7f442aeef973062bb
Daisuke Aritomo <osyoyu@osyoyu.com>
2023-12-21 01:06:58 +0900

    Allow Markdown tables and other syntax

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,7 +7,7 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find_by!(entry_path: params[:entry_path])
-    @body_html = CommonMarker.render_html(@entry.body, :DEFAULT).html_safe
+    @body_html = CommonMarker.render_html(@entry.body, [:DEFAULT, :HARDBREAKS, :UNSAFE, :FOOTNOTES], [:table]).html_safe
   end
 
   def new
