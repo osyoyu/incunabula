4a40af6cfea320351c048aeb1922c7661bf17d92
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-01-04 23:39:58 +0900

    Nicer route for editing

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :entries, path: '/breakpoint', only: [:index, :new, :edit, :create, :update] do
+  resources :entries, path: '/breakpoint', only: [:index, :new, :create, :update] do
     collection do
       get '/*entry_path', to: 'entries#show', as: 'entry', constraints: { entry_path: %r|\d{4}/\d{2}/\d{2}/\d{6}| }
+      get '/*entry_path/edit', to: 'entries#edit', as: 'entry_edit', constraints: { entry_path: %r|\d{4}/\d{2}/\d{2}/\d{6}| }
     end
   end
 end
