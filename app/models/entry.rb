c3b822f6d64b3a82233d1cc57ed751b24f00d1b3
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-01-13 00:43:38 +0900

    Move HTML generation into Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -9,4 +9,8 @@ class Entry < ApplicationRecord
   def published_at_formatted
     published_at.strftime('%Y/%m/%d')
   end
+
+  def render_to_html
+    CommonMarker.render_html(self.body, [:DEFAULT, :HARDBREAKS, :UNSAFE, :FOOTNOTES], [:table]).html_safe
+  end
 end
