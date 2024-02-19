11fc33259fbac29564fb50bd3c6c324a22985e48
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-02-20 00:06:37 +0900

    Show image embed tag

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,12 @@ class ImagesController < ApplicationController
 
   def show
     @image = Image.find(params[:id])
+    @embed_tag = <<~__EOS__
+    <div style="font-size: 14px; text-align: center;">
+    <a href="#{@image.public_url}"><img src="#{@image.public_url(width: 480)}" style="max-width: min(480px, 90%); border: 1px solid #000;" /></a><br />
+    (caption)
+    </div>
+    __EOS__
   end
 
   def new
