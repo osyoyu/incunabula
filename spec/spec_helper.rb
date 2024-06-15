ab85f8b09ec270aa3e48b9e1391fc1a0544c0244
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-06-15 14:45:12 +0900

    rspec: Disable warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ RSpec.configure do |config|
 
   config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
-  config.warnings = true
   if config.files_to_run.one?
     config.default_formatter = 'doc'
   end
